aa3acbc9b539c6e673eee2ae48c8ec9d0365a28e
jml04 <jannematti04@gmail.com>
2022-03-30 11:06:31 +0200

    neu

--- src/planespotter/Controller.java
+++ src/planespotter/Controller.java
@@ -6,6 +6,9 @@ import planespotter.display.*;
 import javax.swing.*;
 import java.lang.module.Configuration;
 import java.util.HashMap;
+import java.util.List;
+
+import static planespotter.Supplier.fr24get;
 
 public class Controller {
 
@@ -54,13 +57,13 @@ public class Controller {
         framesvisible.put(MapView.class, false);
     }
 
-    public static void setFramevisible (Class key, boolean visible) throws FrameNotFoundException {
+    public static void setFrameVisible (Class key, boolean visible) throws FrameNotFoundException {
         if (framesvisible.containsKey(key))
             framesvisible.replace(key, visible);
         else throw new FrameNotFoundException();
     }
 
-    public static boolean getFramevisible (Class key) throws FrameNotFoundException {
+    public static boolean getFrameVisible (Class key) throws FrameNotFoundException {
         if (framesvisible.containsKey(key))
             return framesvisible.get(key);
         else throw new FrameNotFoundException();
@@ -84,6 +87,23 @@ public class Controller {
                                         frame.getHeading(),
                                         frame.getAltitude());
         return point;
+    }
+
+    /**
+     * creates a Frame with deserializer
+     *
+     */
+    public List<Frame> getFrames (){
+        try {
+            Deserializer ds = new Deserializer();
+            List<String> list = ds.stringMagic(fr24get());
+            List<Frame> frames = ds.deserialize(list);
+            return frames;
+        } catch (Exception e) {
+            // Auto-generated catch block
+            e.printStackTrace();
+        }
+        return null;
     }
 
 
