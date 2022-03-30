c8c392ea3ca4438dd26fd061a572191c4fd80c21
Mecki-Messer <lukas.roecher@web.de>
2022-03-30 10:17:46 +0200

    Created Class for SQL Querrys

---
+++ src/planespotter/SqlQuerrys.java
@@ -0,0 +1,20 @@
+package planespotter;
+
+public class SqlQuerrys {
+	
+	public String querry1 = "";
+	public String querry2 = "";
+	public String querry3 = "";
+	public String querry4 = "";
+	public String querry5 = "";
+	public String querry6 = "";
+	public String querry7 = "";
+	public String querry8 = "";
+	public String querry9 = "";
+	public String querry10 = "";
+	public String querry11 = "";
+	public String querry12 = "";
+	public String querry13 = "";
+	public String querry14 = "";
+
+}
