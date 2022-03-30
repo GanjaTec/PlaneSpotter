2433329919b32eda6fd30ced23002790f9cfc458
Mecki-Messer <lukas.roecher@web.de>
2022-03-30 10:49:03 +0200

    added new deserialize method

--- src/planespotter/Deserializer.java
+++ src/planespotter/Deserializer.java
@@ -49,11 +49,23 @@ public class Deserializer {
 
 	
 	//Convert Json to Frame-Objects
-	public List<Frame> deserialize(List<String> data) {
+	public List<Frame> deserializeJSON(List<String> data) {
 		List<Frame> frames = new ArrayList<Frame>();
 		Gson gson = new Gson();
 		for(String s : data) {
 			Frame frame = gson.fromJson(s, Frame.class);
+			frames.add(frame);
+		}
+		return frames;
+	}
+	
+	public List<Frame> deserialize(List<String> data) {
+		List<Frame> frames = new ArrayList<Frame>();
+		for(String row : data) {
+			
+			String[] r = row.split(",");
+			Frame frame = new Frame(r[0], Double.parseDouble(r[1]), Double.parseDouble(r[2]), Integer.parseInt(r[3]), Integer.parseInt(r[4]), Integer.parseInt(r[5]), Integer.parseInt(r[6]),
+					r[7], r[8], r[9], Integer.parseInt(r[10]), r[11], r[12], r[13], r[14], r[15], r[16], r[17], r[18]);
 			frames.add(frame);
 		}
 		return frames;
