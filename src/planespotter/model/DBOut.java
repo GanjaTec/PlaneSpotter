e006580af941ee572cb337ef12ae6854006f9326
Mecki-Messer <lukas.roecher@web.de>
2022-03-31 17:26:01 +0200

    Added Doc Comments

--- src/planespotter/model/DBOut.java
+++ src/planespotter/model/DBOut.java
@@ -8,8 +8,24 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
+/**
+ * @author Lukas
+ *
+ *
+ * This class is used to get Output from the DB
+ * It relies heavily on planespotter.SQLQuerrys
+ *
+ */
 public class DBOut {
 
+	
+	/**
+	 * This method is used to querry the DB
+	 * it takes a String and returns a ResultSet
+	 * 
+	 * @param querry String to use for the Querry
+	 * @return ResultSet containing the querried Data
+	 */
 	public ResultSet querryDB(String querry) {
 		ResultSet rs;
 		try {
@@ -26,6 +42,14 @@ public class DBOut {
 		return rs;
 	}
 
+	/**
+	 * This method is a quick botch because i messed up the Airports Table in the DB
+	 * It takes A string containing the Coordinates received from the DB and converts it
+	 * into a Position Object
+	 * 
+	 * @param coords The string containing the Coords
+	 * @return Position containing Latitude and Longitude
+	 */
 	private Position convertCoords(String coords) {	
 		String[] splitCoords = coords.split(",");
 		List<Double> processedCoords = new ArrayList<Double>();
@@ -38,6 +62,16 @@ public class DBOut {
 		return p;
 	}
 
+	/**
+	 * This method is used to Querry the DB for Airline by its assigned ICAO Tag
+	 * It takes a String containing the ICAO Tag and returns
+	 * an Airline Object
+	 * 
+	 * 
+	 * @param tag the ICAO-Tag used in the Querry
+	 * @return Airline Object
+	 * @throws SQLException
+	 */
 	public Airline getAirlineByTag(String tag) throws SQLException {
 		Airline a = null;
 		ResultSet rs = querryDB(SqlQuerrys.getAirlineByTag + tag);
@@ -48,6 +82,15 @@ public class DBOut {
 		return a;
 	}
 
+	/**
+	 * This Method is used to Querry both the Departure (src) and the Arrival (dst)
+	 * Airports of a Flight and returns a List containing two Airport Objects 
+	 * 
+	 * @param srcAirport String containing the Departure Airports IATA Tag
+	 * @param destAirport String containing the Arrival Airports IATA Tag
+	 * @return List<Airport> the list containing the Airport Objects
+	 * @throws SQLException
+	 */
 	public List<Airport> getAirports(String srcAirport, String destAirport) throws SQLException{
 		List<Airport> aps = new ArrayList<Airport>();
 		ResultSet rsSrc = querryDB(SqlQuerrys.getAirportByTag + srcAirport);
@@ -73,6 +116,14 @@ public class DBOut {
 		return aps;
 	}
 	
+	/**
+	 * This Method is used to Querry a single Plane by its ICAO Tag
+	 * It takes a String containing the ICAO Tag and returns a Plane Object
+	 * 
+	 * @param icao Strin containing the ICAO Tag
+	 * @return Plane the Object containing all Information about the Plane
+	 * @throws SQLException
+	 */
 	public Plane getPlaneByICAO(String icao) throws SQLException {
 		Plane p = null;
 		ResultSet rs = querryDB(SqlQuerrys.getPlaneByICAO + icao);
@@ -85,6 +136,15 @@ public class DBOut {
 		return p;
 	}
 
+	/**
+	 * This Method is used to Querry all Datapoints belonging to a single Flight
+	 * It takes an int FlightID and returns a HashMap<Long, DataPoint> containing the Tracking Data
+	 * 
+	 * 
+	 * @param flightID int representing the Flights Database ID
+	 * @return HashMap<Long, DataPoint> containing all Datapoints keyed with Timestamp
+	 * @throws SQLException
+	 */
 	public HashMap<Long, DataPoint> getTrackingByFlight(int flightID) throws SQLException {
 		HashMap<Long ,DataPoint> dps = new HashMap<Long, DataPoint>();
 		ResultSet rs = querryDB(SqlQuerrys.getTrackingByFlight + flightID);
@@ -98,6 +158,22 @@ public class DBOut {
 
 	}
 	
+	/**
+	 * This Method is used to retrieve ALL flights and their representative Data from the DB
+	 * It takes no Parameters and returns a List<Flight> containing all Flight Objects
+	 * 
+	 * It relies on a lot of other Methods in this class to gather the Objects needed to construct the
+	 * Flight objects.
+	 * 
+	 * TODO Fix Bug causing OutOfMemoryError
+	 * This will be kinda hard, the method constructs a massive List
+	 * that is way to big to hold in memory
+	 * 
+	 * see errorlog "hs_err_pid30296.log" in the projects root directory
+	 * 
+	 * @return List<Flight> containing all Flight Objects
+	 * @throws SQLException
+	 */
 	public List<Flight> getAllFlights() throws SQLException {
 		List<Flight> flights = new ArrayList<Flight>();
 
