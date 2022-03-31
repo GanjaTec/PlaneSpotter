89fd7b1a9ca2e2e77d8e493272dc42187f3ca961
Mecki-Messer <lukas.roecher@web.de>
2022-03-31 17:26:46 +0200

    Added Class for ThreadPool and Methods

---
+++ src/planespotter/model/Paralel.java
@@ -0,0 +1,24 @@
+package planespotter.model;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+public class Paralel {
+	private int poolsize = 5;
+	private ScheduledExecutorService exe = Executors.newScheduledThreadPool(poolsize);
+
+	public Paralel() {
+
+
+	}
+
+	public void startThreads() throws InterruptedException {
+		for(int i=0; i <= poolsize; i++) {
+			Supplier s = new Supplier();
+			exe.scheduleAtFixedRate(s, 0, 5, TimeUnit.MINUTES);
+			TimeUnit.SECONDS.sleep(30);
+		}
+
+	}
+}
