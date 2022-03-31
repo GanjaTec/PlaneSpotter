6ce93686aef075abc491979da0d248c6fd65971a
jml04 <jannematti04@gmail.com>
2022-03-31 14:29:34 +0200

    new version for GUI, Main, Controller, TreeFactory

--- src/planespotter/Controller.java
+++ src/planespotter/Controller.java
@@ -1,22 +1,12 @@
 package planespotter;
 
-import planespotter.Exceptions.JFrameNotFoundException;
 import planespotter.dataclasses.*;
 import planespotter.display.*;
 import planespotter.model.DBOut;
-import planespotter.model.Deserializer;
 import planespotter.model.TreeFactory;
-
-import javax.swing.*;
-import javax.swing.tree.DefaultMutableTreeNode;
-import javax.swing.tree.TreeNode;
-import java.lang.module.Configuration;
 import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.List;
-import java.util.ListIterator;
-
-import static planespotter.model.Supplier.fr24get;
 
 public class Controller {
 
