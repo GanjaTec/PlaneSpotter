1f6ad6f17a535bf25c81a0c9af73c4825619e6db
Mecki-Messer <lukas.roecher@web.de>
2022-04-02 01:51:27 +0200

    created empty JUnit tests

---
+++ src/planespotter/model/DBInTest.java
@@ -0,0 +1,14 @@
+package planespotter.model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class DBInTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/planespotter/model/DBOutTest.java
@@ -0,0 +1,14 @@
+package planespotter.model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class DBOutTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
