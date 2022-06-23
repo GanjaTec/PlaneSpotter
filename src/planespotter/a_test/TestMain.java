fff7e350f685819c5b9320f148e921d1bed55173
Mecki-Messer <lukas.roecher@web.de>
2022-06-23 18:33:45 +0200

    Small Stuff
    Test Decoder
    .gitignore

--- src/planespotter/a_test/TestMain.java
+++ src/planespotter/a_test/TestMain.java
@@ -1,9 +1,13 @@
 package planespotter.a_test;
 
+import java.security.AccessController;
 import java.util.concurrent.TimeUnit;
 
 import org.jetbrains.annotations.TestOnly;
 import org.junit.jupiter.api.Test;
+import planespotter.SupplierMain;
+import planespotter.controller.Scheduler;
+import planespotter.model.nio.ADSBSupplier;
 import planespotter.model.nio.Paralel;
 
 @TestOnly
@@ -11,7 +15,14 @@ public class TestMain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Paralel p = new Paralel();
+		ADSBSupplier adsb = new ADSBSupplier("localhost", 47806, true);
+		Scheduler s = new Scheduler();
+		s.exec(adsb, "Supplier");
+		//s.exec(()-> SupplierMain.main(null), "supllier");
+
+
+
+		/*Paralel p = new Paralel();
 		
 		try {
 			while (true) {
@@ -21,7 +32,7 @@ public class TestMain {
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}
+		}*/
 	}
 
 }
