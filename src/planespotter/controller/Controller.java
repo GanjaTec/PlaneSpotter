e3d03450917991632fbf42f90818b63d92c65ef7
jml04 <jannematti04@gmail.com>
2022-08-31 16:24:17 +0200

    bitmap file '.bmp' can be loaded now

--- src/planespotter/controller/Controller.java
+++ src/planespotter/controller/Controller.java
@@ -293,7 +293,7 @@ public abstract class Controller {
                 // significance map should be improved
                 case MAP_SIGNIFICANCE -> showSignificanceMap(mapManager, dbOut);
                 // heatmap should be implemented
-                case MAP_HEATMAP -> {  }
+                case MAP_HEATMAP -> showBitmap(null, null);
             }
         } finally {
             done(false);
@@ -555,44 +555,43 @@ public abstract class Controller {
     }
 
     public void saveFile() {
-
-        JFileChooser fileChooser; Rectangle rect; File selected;
-
         getUI().showLoadingScreen(true);
         setLoading(true);
 
         // TODO visible rect beim repainten speichern
         // TODO 2.Möglichkeit: center und zoom speichern
-
         try {
             ViewType currentViewType = getUI().getCurrentViewType();
             String extensions = currentViewType == MAP_HEATMAP ? ".bmp" : ".pls";
-            fileChooser = MenuModels.fileSaver(getUI().getWindow(), extensions);
-            rect = /*(gui.getCurrentVisibleRect() != null) ? gui.getCurrentVisibleRect() :*/ null;
-            selected = fileChooser.getSelectedFile();
+            JFileChooser fileChooser = MenuModels.fileSaver(getUI().getWindow(), extensions);
+            Rectangle rect = /*(gui.getCurrentVisibleRect() != null) ? gui.getCurrentVisibleRect() :*/ null;
+            File selected = fileChooser.getSelectedFile();
             if (selected == null) {
                 done(false);
                 return;
             }
-            String warningMsg = "Couldn't write image file, please use \nthe correct file type ";
+            String warningMsg = "Couldn't write file, please check the file type!";
             switch (currentViewType) {
                 case MAP_HEATMAP -> {
                     Component bottom = getUI().getLayerPane().getBottom();
                     if (selected.getName().endsWith(".bmp") && bottom instanceof ZoomPane zoomPane) {
-                        FileWizard.getFileWizard().writeBitmapImg(zoomPane.getContent(), BufferedImage.TYPE_BYTE_GRAY, selected);
+                        // writing Bitmap image with FileWizard
+                        Image bitmap = zoomPane.getContent();
+                        FileWizard.getFileWizard().writeBitmapImg(bitmap, BufferedImage.TYPE_BYTE_GRAY, selected);
                     } else {
-                        getUI().showWarning(Warning.INVALID_DATA, warningMsg + "'.bmp'");
+                        getUI().showWarning(Warning.INVALID_DATA, warningMsg + extensions);
                     }
                 }
-                default -> {
+                case MAP_TRACKING, MAP_TRACKING_NP, MAP_FROMSEARCH, MAP_SIGNIFICANCE -> {
                     if (selected.getName().endsWith(".pls")) {
                         // new MapData object with loadedData, view type and visible rectangle
                         MapData mapData = new MapData(getDataList(), currentViewType, rect);
                         FileWizard.getFileWizard().savePlsFile(mapData, selected);
                     } else {
-                        getUI().showWarning(Warning.INVALID_DATA, warningMsg + ".pls");
+                        getUI().showWarning(Warning.INVALID_DATA, warningMsg + extensions);
                     }
                 }
+                default -> getUI().showWarning(Warning.NOT_SUPPORTED_YET, "These filetypes are not savable yet!");
             }
         } catch (DataNotFoundException | IOException e) {
             handleException(e);
@@ -609,11 +608,18 @@ public abstract class Controller {
             if (file == null) {
                 return;
             }
-            FileWizard fileWizard = FileWizard.getFileWizard();
-            MapData loaded = fileWizard.loadPlsFile(file);
-            setDataList(loaded.data());
-            show(loaded.viewType());
-        } catch (FileNotFoundException | InvalidDataException e) {
+            String filename = file.getName();
+            if (filename.endsWith(".bmp")) {
+                BufferedImage buf = ImageIO.read(file);
+                showBitmap(null, buf);
+                // TODO: 31.08.2022 show Bitmap from File or Image
+            } else if (filename.endsWith(".pls")) {
+                FileWizard fileWizard = FileWizard.getFileWizard();
+                MapData loaded = fileWizard.loadPlsFile(file);
+                setDataList(loaded.data());
+                show(loaded.viewType());
+            }
+        } catch (InvalidDataException | IOException e) {
             handleException(e);
         } finally {
             done(false);
@@ -729,31 +735,37 @@ public abstract class Controller {
         mapManager.createTrackingMap(getDataList(), flight, showPoints);
     }
 
-    public void showBitmap() {
-        String input = getUI().getUserInput("Please enter a grid size (0.1 - 2.0)", 0.5);
-        if (input.isBlank()) {
-            return;
-        }
-        scheduler.exec(() -> {
-            try {
-                setLoading(true);
-                getUI().showLoadingScreen(true);
-                getUI().setViewType(MAP_HEATMAP);
-                float gridSize = Float.parseFloat(input);
-                if (gridSize < 0.05) {
-                    getUI().showWarning(Warning.OUT_OF_RANGE, "grid size must be 0.05 or higher!");
-                    return;
-                }
-                Bitmap bitmap = new Statistics().globalPositionBitmap(gridSize);
-                Diagrams.showPosHeatMap(getUI(), bitmap);
-            } catch (NumberFormatException nfe) {
-                getUI().showWarning(Warning.NUMBER_EXPECTED, "Please enter a float value (0.1 - 2.0)");
-            } catch (DataNotFoundException | OutOfMemoryError e) {
-                handleException(e);
-            } finally {
-                done(false);
+    public void showBitmap(@Nullable Bitmap bitmap, @Nullable BufferedImage buf) {
+        if (bitmap == null && buf == null) {
+            String input = getUI().getUserInput("Please enter a grid size (0.025 - 2.0)", 0.5);
+            if (input.isBlank()) {
+                return;
             }
-        }, "Loading Data", false, Scheduler.MID_PRIO, true);
+            scheduler.exec(() -> {
+                try {
+                    setLoading(true);
+                    getUI().showLoadingScreen(true);
+                    getUI().setViewType(MAP_HEATMAP);
+                    float gridSize = Float.parseFloat(input);
+                    if (gridSize < 0.025) {
+                        getUI().showWarning(Warning.OUT_OF_RANGE, "grid size must be 0.025 or higher!");
+                        return;
+                    }
+                    Bitmap bmp = new Statistics().globalPositionBitmap(gridSize);
+                    Diagrams.showPosHeatMap(getUI(), bmp);
+                } catch (NumberFormatException nfe) {
+                    getUI().showWarning(Warning.NUMBER_EXPECTED, "Please enter a float value (0.025 - 10.0)");
+                } catch (DataNotFoundException | OutOfMemoryError e) {
+                    handleException(e);
+                } finally {
+                    done(false);
+                }
+            }, "Loading Data", false, Scheduler.MID_PRIO, true);
+        } else if (bitmap != null) {
+            Diagrams.showPosHeatMap(getUI(), bitmap);
+        } else {
+            Diagrams.showPosHeatMap(getUI(), buf);
+        }
     }
 
     /**
